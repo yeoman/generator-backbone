65c0598b387100bd4d50934dfdd31b7cd5e11718
Andrew Thornton <art27@cantab.net>
2013-04-13 16:25:10 +0100

    Make grunt for yo backbone:all work out of the box for the non-require amd case

--- templates/view.js
+++ templates/view.js
@@ -1,5 +1,7 @@
+/*global <%= _.camelize(appname) %> Backbone JST*/
+
 <%= _.camelize(appname) %>.Views.<%= _.classify(name) %>View = Backbone.View.extend({
 
-  template: JST['<%= jst_path %>']
+    template: JST['<%= jst_path %>']
 
 });
