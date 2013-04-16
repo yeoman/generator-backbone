de14a50fba8e83d91f075a80149ee3d01275b3c2
Pascal Hartig <phartig@rdrei.net>
2013-04-16 13:55:13 +0200

    Removed trailing whitespace in generated Gruntfile

--- app/templates/Gruntfile.js
+++ app/templates/Gruntfile.js
@@ -175,8 +175,7 @@ module.exports = function (grunt) {
                     //uglify2: {} // https://github.com/mishoo/UglifyJS2
                 }
             }
-        },<% } else { %>
-        uglify: {
+        },<% } else { %>uglify: {
             dist: {
                 files: {
                     '<%%= yeoman.dist %>/scripts/main.js': [
