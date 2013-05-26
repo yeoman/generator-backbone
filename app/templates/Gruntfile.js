95642eec2ad115ef1470f6b9c7f79513900e29b1
Thanos Lefteris <alefteris@gmail.com>
2013-05-26 14:15:33 +0300

    Update grunt-contrib-jshint package version, rules

--- app/templates/Gruntfile.js
+++ app/templates/Gruntfile.js
@@ -195,7 +195,7 @@ module.exports = function (grunt) {
                     '<%%= yeoman.dist %>/scripts/main.js': [
                         '<%%= yeoman.app %>/scripts/{,*/}*.js',
                         '.tmp/scripts/templates.js'
-                    ],
+                    ]
                 }
             }
         },<% } %>
