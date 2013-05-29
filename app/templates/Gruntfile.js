7fce0f79546cd9f9daf5879ac74c54391fee8aac
Thanos Lefteris <alefteris@gmail.com>
2013-05-29 12:50:07 +0300

    Gruntfile: Replace hard-coded dist reference with config

--- app/templates/Gruntfile.js
+++ app/templates/Gruntfile.js
@@ -96,7 +96,7 @@ module.exports = function (grunt) {
                 options: {
                     middleware: function (connect) {
                         return [
-                            mountFolder(connect, 'dist')
+                            mountFolder(connect, yeomanConfig.dist)
                         ];
                     }
                 }
