c3f3e1b85518c66e0b9b7da253d04d6a289e827b
Revath S Kumar <rsk@revathskumar.com>
2013-09-16 23:36:31 +0530

    closes #141 : livereload on template files

--- app/templates/Gruntfile.js
+++ app/templates/Gruntfile.js
@@ -50,7 +50,8 @@ module.exports = function (grunt) {
                     '<%%= yeoman.app %>/*.html',
                     '{.tmp,<%%= yeoman.app %>}/styles/{,*/}*.css',
                     '{.tmp,<%%= yeoman.app %>}/scripts/{,*/}*.js',
-                    '<%%= yeoman.app %>/images/{,*/}*.{png,jpg,jpeg,gif,webp}'
+                    '<%%= yeoman.app %>/images/{,*/}*.{png,jpg,jpeg,gif,webp}',
+                    '<%%= yeoman.app %>/scripts/templates/*.{ejs,mustache,hbs}'
                 ]
             }<% if (templateFramework === 'mustache') { %>,
             mustache: {
