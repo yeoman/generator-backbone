81b0e85ca94a76cd0476f2aedceee391296e51b8
Revath S Kumar <rsk@revathskumar.com>
2013-11-03 00:09:19 +0530

    fixes #165

--- app/templates/Gruntfile.js
+++ app/templates/Gruntfile.js
@@ -296,7 +296,8 @@ module.exports = function (grunt) {
                         '*.{ico,txt}',
                         '.htaccess',
                         'images/{,*/}*.{webp,gif}',
-                        'styles/fonts/{,*/}*.*'
+                        'styles/fonts/{,*/}*.*',
+                        'bower_components/sass-bootstrap/fonts/*.*'
                     ]
                 }]
             }
