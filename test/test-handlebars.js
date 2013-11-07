09bd7f1bd5d83ac290d1eac5bbbeec6e117a8f68
Revath S Kumar <rsk@revathskumar.com>
2013-11-07 18:02:27 +0530

    Adding view tests for handlebars and mustache

---
+++ test/test-handlebars.js
@@ -0,0 +1,47 @@
+/*global describe:true, beforeEach:true, it:true */
+'use strict';
+var path    = require('path');
+var helpers = require('yeoman-generator').test;
+var assert  = require('assert');
+
+describe('Backbone generator with handlebars', function () {
+  beforeEach(function (done) {
+    helpers.testDirectory(path.join(__dirname, './temp'), function (err) {
+      if (err) {
+        return done(err);
+      }
+      this.backbone = {};
+      this.backbone.app = helpers.createGenerator('backbone:app', [
+        '../../app', [
+          helpers.createDummyGenerator(),
+          'mocha:app'
+        ]
+      ],['temp'], {'template-framework': 'handlebars'});
+      this.backbone.app.options['skip-install'] = true;
+
+      helpers.mockPrompt(this.backbone.app, {
+        features: ['compassBootstrap'],
+        includeRequireJS: false
+      });
+
+      done();
+    }.bind(this));
+
+  });
+
+  describe('Backbone View', function () {
+    it('creates backbone view', function (done) {
+      var view = helpers.createGenerator('backbone:view', ['../../view'], ['foo']);
+
+      this.backbone.app.run({}, function () {
+        view.run([], function () {
+          helpers.assertFiles([
+            ['app/scripts/views/foo.js', /Views.FooView = Backbone.View.extend\(\{(.|\n)*app\/scripts\/templates\/foo.hbs/],
+            'app/scripts/templates/foo.hbs'
+          ]);
+        });
+        done();
+      });
+    });
+  });
+});
