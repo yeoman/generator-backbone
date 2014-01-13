a0c2487f83549464efae46b98f2270d4044d9874
Jaseem Abid <jaseemabid@gmail.com>
2014-01-13 16:47:05 +0530

    Strip out comments, just the skeleton

--- templates/model.js
+++ templates/model.js
@@ -7,34 +7,20 @@
 
     <%= _.camelize(appname) %>.Models.<%= _.classify(name) %>Model = Backbone.Model.extend({
 
-        // The defaults hash (or function) can be used to specify the default
-        // attributes for your model. When creating an instance of the model,
-        // any unspecified attributes will be set to their default value.
+        url: '',
+
         initialize: function() {
         },
 
-        // The defaults hash (or function) can be used to specify the default
-        // attributes for your model. When creating an instance of the model,
-        // any unspecified attributes will be set to their default value.
         defaults: {
         },
 
-        // Override it with your custom validation logic. By default validate is
-        // called before save, but can also be called before set if
-        // {validate:true} is passed. Return an error object or message if the
-        // validation fails and else nothing.
         validate: function(attrs, options) {
             // if (attrs.end < attrs.start) {
             //   return "can't end before it starts";
             // }
         },
 
-        // Returns the relative URL where the model's resource would be located
-        // on the server.
-        url: '',
-
-        // The function is passed the raw response object, and should return the
-        // attributes hash to be set on the model.
         parse: function(response, options)  {
             return response;
         }
