3d810bd5dde80e69ae4849941f82693b926a75cf
Jaseem Abid <jaseemabid@gmail.com>
2014-01-15 11:19:58 +0530

    Skeleton view files

--- templates/coffeescript/requirejs/view.coffee
+++ templates/coffeescript/requirejs/view.coffee
@@ -6,3 +6,17 @@ define [
 ], ($, _, Backbone, JST) ->
   class <%= _.classify(name) %>View extends Backbone.View
     template: JST['<%= jst_path %>']
+
+    tagName: 'div'
+
+    id: ''
+
+    className: ''
+
+    events: {}
+
+    initialize: () ->
+        @listenTo @model, 'change', @render
+
+    render: () ->
+        @$el.html @template(@model.toJSON())
--- templates/requirejs/view.js
+++ templates/requirejs/view.js
@@ -9,7 +9,23 @@ define([
     'use strict';
 
     var <%= _.classify(name) %>View = Backbone.View.extend({
-        template: JST['<%= jst_path %>']
+        template: JST['<%= jst_path %>'],
+
+        tagName: 'div',
+
+        id: '',
+
+        className: '',
+
+        events: {},
+
+        initialize: function () {
+            this.listenTo(this.model, 'change', this.render);
+        },
+
+        render: function () {
+            this.$el.html(this.template(this.model.toJSON()));
+        }
     });
 
     return <%= _.classify(name) %>View;
