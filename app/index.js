e59500801ca6e487fab9b57019679455e0cc20de
Gil Barbara <gilbarbara@gmail.com>
2014-01-26 21:46:05 -0200

    fixed generator argument ignored
    generator was using the directory path as default

--- app/index.js
+++ app/index.js
@@ -8,6 +8,9 @@ var scriptBase = require('../script-base');
 var Generator = module.exports = function Generator(args, options, config) {
   yeoman.generators.Base.apply(this, arguments);
 
+  this.argument('appname', { type: String, required: false });
+  this.appname = this.appname || path.basename(process.cwd());
+
   this.env.options.appPath = this.options.appPath || 'app';
   this.config.set('appPath', this.env.options.appPath);
 
