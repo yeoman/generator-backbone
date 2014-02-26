75d0d2c517bd30d81530142449fd4909ac2acf12
Gil Barbara <gilbarbara@gmail.com>
2014-02-26 19:49:24 -0300

    fixed `grunt test` being triggered while running `grunt serve`

--- app/templates/Gruntfile.js
+++ app/templates/Gruntfile.js
@@ -74,11 +74,7 @@ module.exports = function (grunt) {
                     '<%%= yeoman.app %>/scripts/templates/*.ejs'
                 ],
                 tasks: ['jst']
-            }<% } %>,
-            test: {
-                files: ['<%%= yeoman.app %>/scripts/{,*/}*.js', 'test/spec/**/*.js'],
-                tasks: ['test:true']
-            }
+            }<% } %>
         },
         connect: {
             options: {
@@ -403,6 +399,21 @@ module.exports = function (grunt) {
             'open:server',
             'watch'
         ]);
+    });
+
+    grunt.registerTask('watch:test', function() {
+        var config = {
+            options: {
+                interrupt: true
+            },
+            test: {
+                files: ['<%= yeoman.app %>/scripts/{,*/}*.js', 'test/spec/**/*.js'],
+                tasks: ['test:true']
+            }
+        };
+
+        grunt.config('watch', config);
+        grunt.task.run('watch');
     });
 
     grunt.registerTask('test', function (isConnected) {
